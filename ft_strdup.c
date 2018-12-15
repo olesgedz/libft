80f611c31a94a84fa928d93cf21fe1670e39dcf4
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-15 11:04:51 +0300

    hello

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 16:07:42 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/15 10:36:06 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/15 10:48:14 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ char	*ft_strdup(const char *s1)
 {
 	char *new;
 
-	if (!(new = (char*)malloc(sizeof(char) * ft_strlen(src) + 1)))
+	if (!(new = (char*)malloc(sizeof(char) * ft_strlen(s1) + 1)))
 		return (NULL);
-	ft_strcpy(new, src);
+	ft_strcpy(new, s1);
 	return (new);
 }
--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 17:32:09 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/15 10:35:05 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/15 10:47:03 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t		ft_strlen(const char *s)
 	int count;
 
 	count = 0;
-	while (str[count] != '\0')
+	while (s[count] != '\0')
 		count++;
 	return (count);
 }
