284dd7afdb176b4b220f5c00a3e5c7732d35c75f
olesgedz <olesgedz@olesgedzs-MacBook-Pro-3.local>
2018-12-23 11:14:35 +0300

    kk

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 20:53:34 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/09 21:07:47 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/23 11:14:28 by olesgedz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new;
 
-	if (!(new = (t_list *)malloc(sizeof(t_list) * content_size)))
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
 		return (NULL);
 	if (!content)
 	{
@@ -27,7 +27,11 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	else
 	{
 		if (!(new->content = (void *)malloc(content_size)))
+		{
+			free(new);
+			new = NULL;
 			return (NULL);
+		}
 		ft_memcpy(new->content, content, content_size);
 		new->content_size = content_size;
 	}
