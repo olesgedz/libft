e9ea8ff9e2dddc4c6bc21b350094c82ed58cc87c
olesgedz <olesgedz@rambler.ru>
2019-01-02 23:11:13 +0300

    ft_memccpy cleanup

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 19:40:06 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/08 20:13:02 by jblack-b         ###   ########.fr       */
+/*   Updated: 2019/01/02 23:04:25 by olesgedz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,14 @@
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t	i;
-	int		flag;
 
-	flag = 0;
 	i = 0;
 	while (i < n)
 	{
 		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
 		if (((unsigned char*)dst)[i] == (unsigned char)c)
-		{
-			flag = 1;
-			break ;
-		}
+				return (((unsigned char*)dst) + i + 1);
 		i++;
 	}
-	if (flag == 1)
-		return (((unsigned char*)dst) + i + 1);
 	return (NULL);
 }
