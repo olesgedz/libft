a3a65f870629f984b17300703e9634943d201371
olesgedz <olesgedz@rambler.ru>
2019-10-11 16:34:59 +0300

    30

---
+++ sources/array/ft_2darray_uchar_free.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_2darray_uchar_free.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/11 16:07:18 by jblack-b          #+#    #+#             */
+/*   Updated: 2019/10/11 16:18:22 by jblack-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_2darray_uchar_free(unsigned char ***tab)
+{
+	unsigned char **names;
+
+	if (tab)
+	{
+		names = *tab;
+		while (*names != NULL)
+			free(*names++);
+	}
+}
